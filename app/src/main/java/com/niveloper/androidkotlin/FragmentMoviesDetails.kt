7171c520d2b0f3fb7cd1071925a125be584d3b3b
EdwardNee <nieduard_01@mail.ru>
2021-01-12 22:50:32 +0600

    back_text added

--- app/src/main/java/com/niveloper/androidkotlin/FragmentMoviesDetails.kt
+++ app/src/main/java/com/niveloper/androidkotlin/FragmentMoviesDetails.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -28,6 +29,11 @@ class FragmentMoviesDetails : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_movies_details, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val back_tv =view.findViewById<TextView>(R.id.)
     }
 
     companion object {
