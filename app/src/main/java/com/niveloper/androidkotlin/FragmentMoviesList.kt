c6c5c1485150dcdd27d0de8bd46d6bfdf07233c4
EdwardNee <nieduard_01@mail.ru>
2021-01-13 00:20:39 +0600

    setOnClickListener: go to FragmentMoviesDetails.kt

--- app/src/main/java/com/niveloper/androidkotlin/FragmentMoviesList.kt
+++ app/src/main/java/com/niveloper/androidkotlin/FragmentMoviesList.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -39,6 +40,13 @@ class FragmentMoviesList : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val movie = view.findViewById<FrameLayout>(R.id.avengers_framelayout).setOnClickListener {
+            val frag = FragmentMoviesDetails()
+            activity!!.supportFragmentManager.beginTransaction()
+                .replace(this.id, frag, null)
+                .addToBackStack(null)
+                .commit()
+        }
     }
 
     companion object {
