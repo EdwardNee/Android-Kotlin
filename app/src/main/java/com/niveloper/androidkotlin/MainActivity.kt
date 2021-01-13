99b2b9684432ae45b7b895d4744adf3420ff4291
EdwardNee <nieduard_01@mail.ru>
2021-01-13 13:50:07 +0600

    setContentView(activity_main) and that  works!

--- app/src/main/java/com/niveloper/androidkotlin/MainActivity.kt
+++ app/src/main/java/com/niveloper/androidkotlin/MainActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_movies_list)
+        setContentView(R.layout.activity_main)
         supportFragmentManager.beginTransaction()
             .add(R.id.fragment_container_main, FragmentMoviesList())
             .commit()
