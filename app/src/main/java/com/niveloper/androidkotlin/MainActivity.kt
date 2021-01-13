3e306c3c2e4e60f3956b4e16041a2b9968718dbf
EdwardNee <nieduard_01@mail.ru>
2021-01-13 13:44:21 +0600

    activity_main to setContentVIew

--- app/src/main/java/com/niveloper/androidkotlin/MainActivity.kt
+++ app/src/main/java/com/niveloper/androidkotlin/MainActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_movie_details)
+        setContentView(R.layout.activity_main)
         supportFragmentManager.beginTransaction()
             .add(R.id.fragment_container_main, FragmentMoviesList())
     }
