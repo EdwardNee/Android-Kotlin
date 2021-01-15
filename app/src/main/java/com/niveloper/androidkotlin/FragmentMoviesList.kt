873a6af1927a7c9850b041c26980c6ff431dde52
EdwardNee <nieduard_01@mail.ru>
2021-01-15 23:27:47 +0600

    FragmentMoviesList.kt cleaned up the code

--- app/src/main/java/com/niveloper/androidkotlin/FragmentMoviesList.kt
+++ app/src/main/java/com/niveloper/androidkotlin/FragmentMoviesList.kt
@@ -18,17 +18,6 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class FragmentMoviesList : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -47,25 +36,5 @@ class FragmentMoviesList : Fragment() {
                 .addToBackStack(null)
                 .commit()
         }
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FragmentMoviesList.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FragmentMoviesList().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
