03263a24f1f120c7412237950f887ae632803428
EdwardNee <nieduard_01@mail.ru>
2021-01-18 18:42:10 +0600

    Adapters implemented ListAdapter

--- app/src/main/java/com/niveloper/androidkotlin/AdapterMovieDetails.kt
+++ app/src/main/java/com/niveloper/androidkotlin/AdapterMovieDetails.kt
@@ -3,9 +3,10 @@ package com.niveloper.androidkotlin
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 
-class AdapterMovieDetails : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class AdapterMovieDetails : ListAdapter<ActorData, RecyclerView.ViewHolder>(ActorDiffCallback()) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         TODO("Not yet implemented")
     }
@@ -14,9 +15,6 @@ class AdapterMovieDetails : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
         TODO("Not yet implemented")
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
 }
 
 class ActorViewHolder(view: View) : RecyclerView.ViewHolder(view) {
@@ -24,9 +22,9 @@ class ActorViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 }
 
 /**
- * Callback class for updating data of Actors.
+ * Callback class for updating difference data of Actors.
  */
-class DiffCallback : DiffUtil.ItemCallback<ActorData>() {
+class ActorDiffCallback : DiffUtil.ItemCallback<ActorData>() {
     override fun areItemsTheSame(oldItem: ActorData, newItem: ActorData): Boolean {
         return oldItem.name == newItem.name
     }
--- app/src/main/java/com/niveloper/androidkotlin/AdapterMoviesList.kt
+++ app/src/main/java/com/niveloper/androidkotlin/AdapterMoviesList.kt
@@ -3,9 +3,10 @@ package com.niveloper.androidkotlin
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 
-class AdapterMoviesList : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class AdapterMoviesList : ListAdapter<MovieData, RecyclerView.ViewHolder>(MovieDiffCallback()) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         TODO("Not yet implemented")
     }
@@ -14,9 +15,6 @@ class AdapterMoviesList : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
         TODO("Not yet implemented")
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
 }
 
 class MovieViewHolder(view: View) : RecyclerView.ViewHolder(view) {
@@ -24,9 +22,9 @@ class MovieViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 }
 
 /**
- * Callback class for updating list of movies.
+ * Callback class for updating difference list of movies.
  */
-class DifCallback : DiffUtil.ItemCallback<MovieData>(){
+class MovieDiffCallback : DiffUtil.ItemCallback<MovieData>(){
     override fun areItemsTheSame(oldItem: MovieData, newItem: MovieData): Boolean {
         return oldItem.name == newItem.name
     }
