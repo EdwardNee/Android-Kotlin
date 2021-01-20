7659c39b93937e2e8318f54ddb4ef915239d16c8
EdwardNee <nieduard_01@mail.ru>
2021-01-20 22:48:55 +0600

    fields to MovieViewHolder in AdapterMoviesList.kt

--- app/src/main/java/com/niveloper/androidkotlin/AdapterMoviesList.kt
+++ app/src/main/java/com/niveloper/androidkotlin/AdapterMoviesList.kt
@@ -1,8 +1,11 @@
 package com.niveloper.androidkotlin
 
+import android.media.Rating
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.RatingBar
 import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
@@ -24,11 +27,14 @@ class AdapterMoviesList : ListAdapter<MovieData, MovieViewHolder>(MovieDiffCallb
 
 class MovieViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     private val movieName :TextView = view.findViewById(R.id.movie_naming)
-    private val movieImg :
-    private val
-    private val
-    private val
-    private val
+    private val movieImg : ImageView = view.findViewById(R.id.movie_img)
+    private val movieAging : TextView = view.findViewById(R.id.movie_aging)
+    private val movieGenre: TextView = view.findViewById(R.id.genre_movie)
+    private val movieRating: RatingBar = view.findViewById(R.id.ratingBar_movie)
+    private val movieReviews: TextView = view.findViewById(R.id.reviews_movie)
+    private val movieDuration: TextView = view.findViewById(R.id.movie_duration)
+    private val movieLiked: TextView = view.findViewById(R.id.reaction)
+
     fun onBind(item : MovieData){
 
     }
