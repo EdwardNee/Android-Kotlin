e4097ae7d48cdd72bc47bc4127d49704c2fa01ef
EdwardNee <nieduard_01@mail.ru>
2021-01-20 20:54:37 +0600

    DataStorage.kt filled with 4 films

--- app/src/main/java/com/niveloper/androidkotlin/DataStorage.kt
+++ app/src/main/java/com/niveloper/androidkotlin/DataStorage.kt
@@ -35,8 +35,48 @@ class DataStorage {
                 true,
                 5,
                 "Action, Sci-Fi, Thriller",
-                125,
+                98,
                 97,
+                arrayOf(
+                    ActorData("Robert Downey Jr.", 1, R.drawable.r_d_jr),
+                    ActorData("Chris Evans", 2, R.drawable.movie_c_evans),
+                    ActorData("Mark Ruffalo", 3, R.drawable.mark_rufallo),
+                    ActorData("Chris Hemsworth", 4, R.drawable.c_hemsworth),
+                )
+            ),
+
+            MovieData(
+                "Black Window",
+                3,
+                R.drawable.bwindow_movie,
+                R.drawable.bg_fragment,
+                13,
+                "At birth the Black Widow (aka Natasha Romanova) is given to the KGB, which grooms her to become its ultimate operative. When the U.S.S.R. breaks up, the government tries to kill her as the action moves to present-day New York, where she is a freelance operative.",
+                false,
+                4,
+                "Action, Adventure, Sci-Fi",
+                38,
+                102,
+                arrayOf(
+                    ActorData("Robert Downey Jr.", 1, R.drawable.r_d_jr),
+                    ActorData("Chris Evans", 2, R.drawable.movie_c_evans),
+                    ActorData("Mark Ruffalo", 3, R.drawable.mark_rufallo),
+                    ActorData("Chris Hemsworth", 4, R.drawable.c_hemsworth),
+                )
+            ),
+
+            MovieData(
+                "Wonder Woman 1984",
+                4,
+                R.drawable.wwoman_movie,
+                R.drawable.bg_fragment,
+                13,
+                "A secret agent embarks on a dangerous, time-bending mission to prevent the start of World War III.",
+                false,
+                5,
+                "Action, Adventure, Fantasy",
+                74,
+                120,
                 arrayOf(
                     ActorData("Robert Downey Jr.", 1, R.drawable.r_d_jr),
                     ActorData("Chris Evans", 2, R.drawable.movie_c_evans),
