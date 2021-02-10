34a6d1c76b92493f52360bc7f3cb6fd09189b55b
EdwardNee <nieduard_01@mail.ru>
2021-02-10 13:31:17 +0600

    MovieListModelFactory.kt implemented

--- app/src/main/java/com/niveloper/androidkotlin/movielist/MovieListModelFactory.kt
+++ app/src/main/java/com/niveloper/androidkotlin/movielist/MovieListModelFactory.kt
@@ -3,9 +3,11 @@ package com.niveloper.androidkotlin.movielist
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.niveloper.androidkotlin.data.JsonLoadRepository
+import java.lang.IllegalArgumentException
 
 class MovieListModelFactory(private val repo : JsonLoadRepository) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T = when(modelClass){
-        
+        MovieListModelFactory::class.java -> MovieListModelFactory(repo)
+        else -> throw IllegalArgumentException("$modelClass is not registered")
     } as T
 }
---
+++ app/src/main/java/com/niveloper/androidkotlin/movielist/MovieListViewModel.kt
@@ -0,0 +1,6 @@
+package com.niveloper.androidkotlin.movielist
+
+import com.niveloper.androidkotlin.data.JsonLoadRepository
+
+class MovieListViewModel(private val repo: JsonLoadRepository) {
+}
