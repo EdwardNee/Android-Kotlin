27342fb8569af3b218aec52c602653eebeb1b95a
EdwardNee <nieduard_01@mail.ru>
2021-02-10 13:40:14 +0600

    MovieListViewModel.kt added

--- app/src/main/java/com/niveloper/androidkotlin/movielist/MovieListViewModel.kt
+++ app/src/main/java/com/niveloper/androidkotlin/movielist/MovieListViewModel.kt
@@ -1,6 +1,24 @@
 package com.niveloper.androidkotlin.movielist
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.niveloper.androidkotlin.data.JsonLoadRepository
+import com.niveloper.androidkotlin.datastore.MovieData
+import kotlinx.coroutines.launch
 
-class MovieListViewModel(private val repo: JsonLoadRepository) {
+class MovieListViewModel(private val repo: JsonLoadRepository) : ViewModel() {
+    private val _movies = MutableLiveData<List<MovieData?>>()
+    val movies : LiveData<List<MovieData?>> get() = _movies
+
+    init {
+        loadMovies()
+    }
+
+    private fun loadMovies(){
+        viewModelScope.launch {
+            _movies.value = repo.loadMovies()
+        }
+    }
 }
