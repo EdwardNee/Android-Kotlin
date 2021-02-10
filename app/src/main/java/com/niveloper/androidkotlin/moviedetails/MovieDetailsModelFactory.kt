2faeb5f8679a5f6e08c13465c6932b6e49cdefb1
EdwardNee <nieduard_01@mail.ru>
2021-02-10 12:39:28 +0600

    Rename classes MovieDetails

---
+++ app/src/main/java/com/niveloper/androidkotlin/moviedetails/MovieDetailsModelFactory.kt
@@ -0,0 +1,10 @@
+package com.niveloper.androidkotlin.moviedetails
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class MovieDetailsModelFactory : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+
+    }
+}
