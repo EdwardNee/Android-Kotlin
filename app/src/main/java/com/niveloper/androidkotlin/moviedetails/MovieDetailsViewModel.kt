e474d1d5d224d82fd3620131643e96fc3e008999
EdwardNee <nieduard_01@mail.ru>
2021-02-10 12:57:33 +0600

    MovieDetailsViewModel.kt impl

--- app/src/main/java/com/niveloper/androidkotlin/moviedetails/MovieDetailsViewModel.kt
+++ app/src/main/java/com/niveloper/androidkotlin/moviedetails/MovieDetailsViewModel.kt
@@ -1,7 +1,20 @@
 package com.niveloper.androidkotlin.moviedetails
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.niveloper.androidkotlin.data.JsonLoadRepository
+import com.niveloper.androidkotlin.datastore.MovieData
+import kotlinx.coroutines.launch
 
-class MovieDetailsViewModel(private val repo : JsonLoadRepository) : ViewModel() {
+class MovieDetailsViewModel(private val repo: JsonLoadRepository) : ViewModel() {
+    private val _movie = MutableLiveData<MovieData>()
+    val movie: LiveData<MovieData> get() = _movie
+
+    fun loadMovies(movieId: Int) {
+        viewModelScope.launch {
+            _movie.value = repo.loadMovie(movieId)
+        }
+    }
 }
