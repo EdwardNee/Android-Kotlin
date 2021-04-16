ff9d868a8da61bc907bca085c5fbd458d12d0e4d
EdwardNee <nieduard_01@mail.ru>
2021-04-16 13:38:00 +0300

    commented code that is not working

--- app/src/main/java/com/niveloper/androidkotlin/data/api/RetrofitDataSource.kt
+++ app/src/main/java/com/niveloper/androidkotlin/data/api/RetrofitDataSource.kt
@@ -1,32 +1,32 @@
-package com.niveloper.androidkotlin.data.api
-
-import com.niveloper.androidkotlin.data.DataSourceInterface
-import com.niveloper.androidkotlin.datastore.Movie
-import com.niveloper.androidkotlin.datastore.MovieData
-
-class RetrofitDataSource(val api: MovieApi) : DataSourceInterface {
-    override suspend fun loadMovies(): List<MovieData> {
-        api.loadConfiguration();
-        val genres = api.loadGenres().genres
-        return api.loadUpComing(page = 1).results.map { movie ->
-            Movie(
-                id = movie.id,
-                title = movie.title,
-                imageUrl = buildUrl(movie)
-            )
-        }
-    }
-
-    override suspend fun loadMovie(movieId: Int): Movie {
-        api.loadConfiguration();
-    }
-
-    private fun buildUrl(url: String?, size: String?, path: String?): String? {
-        return if (url == null || path == null) {
-            null
-        } else {
-            url.plus(size.takeUnless { it.isNullOrEmpty() } ?: "original")
-                .plus(path)
-        }
-    }
-}
+//package com.niveloper.androidkotlin.data.api
+//
+//import com.niveloper.androidkotlin.data.DataSourceInterface
+//import com.niveloper.androidkotlin.datastore.Movie
+//import com.niveloper.androidkotlin.datastore.MovieData
+//
+//class RetrofitDataSource(val api: MovieApi) : DataSourceInterface {
+//    override suspend fun loadMovies(): List<MovieData> {
+//        api.loadConfiguration();
+//        val genres = api.loadGenres().genres
+//        return api.loadUpComing(page = 1).results.map { movie ->
+//            Movie(
+//                id = movie.id,
+//                title = movie.title,
+//                imageUrl = buildUrl(movie)
+//            )
+//        }
+//    }
+//
+//    override suspend fun loadMovie(movieId: Int): Movie {
+//        api.loadConfiguration();
+//    }
+//
+//    private fun buildUrl(url: String?, size: String?, path: String?): String? {
+//        return if (url == null || path == null) {
+//            null
+//        } else {
+//            url.plus(size.takeUnless { it.isNullOrEmpty() } ?: "original")
+//                .plus(path)
+//        }
+//    }
+//}
